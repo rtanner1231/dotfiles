f8bb8b2102e24870cdcdfb3843d0cdf654c641f4
Richard Tanner <rtanner@myron.com>
2024-04-18 12:10:19 -0400

    Add fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+export NVIMQueryKey=Adfsgh344564
+
+
+
+starship init fish | source
+zoxide init fish | source
+fzf --fish | source
