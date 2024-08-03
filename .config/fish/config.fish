f3d41138f476c94f47458e5091c000b217b9ffcb
Richard Tanner <rtanner@myron.com>
2024-08-03 16:39:12 -0400

    Fish config update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,7 +4,8 @@ end
 
 export NVIMQueryKey=Adfsgh344564
 
-
+set -gx $EDITOR "nvim"
+set fzf_dir_opts --bind "ctrl-o:execute($EDITOR {} &> /dev/tty)"
 
 starship init fish | source
 zoxide init fish | source
