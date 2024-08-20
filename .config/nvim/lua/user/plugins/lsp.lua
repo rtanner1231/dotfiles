43665cf1a08f6903bd19d8eae0e0eaf301a20d52
Richard Tanner <rtanner@myron.com>
2024-08-20 09:35:43 -0400

    NVIM injections

--- .config/nvim/lua/user/plugins/lsp.lua
+++ .config/nvim/lua/user/plugins/lsp.lua
@@ -93,6 +93,7 @@ return {
 				--  For example, in C this would take you to the header.
 				map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
+				map("gl", vim.diagnostic.open_float, "[G]oto Diagnostic [L]oc")
 				-- The following two autocommands are used to highlight references of the
 				-- word under your cursor when your cursor rests there for a little while.
 				--    See `:help CursorHold` for information about when this is executed
