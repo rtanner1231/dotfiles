d924b4a89d89472361179fec8f1325cc507072c7
Richard Tanner <rtanner@myron.com>
2025-09-07 15:10:57 -0400

    Add fff.lua

---
+++ .config/nvim/lua/user/plugins/fff.lua
@@ -0,0 +1,28 @@
+return {
+	"dmtrKovalenko/fff.nvim",
+	build = "cargo build --release",
+	-- or if you are using nixos
+	-- build = "nix run .#release",
+	opts = { -- (optional)
+		debug = {
+			enabled = false, -- we expect your collaboration at least during the beta
+			show_scores = false, -- to help us optimize the scoring system, feel free to share your scores!
+		},
+		keymaps = {
+			move_up = { "<Up>", "<C-p>", "<tab>" },
+			move_down = { "<Down>", "<C-n>", "<S-tab>" },
+		},
+	},
+	-- No need to lazy-load with lazy.nvim.
+	-- This plugin initializes itself lazily.
+	lazy = false,
+	keys = {
+		{
+			"<leader>ff", -- try it if you didn't it is a banger keybinding for a picker
+			function()
+				require("fff").find_files()
+			end,
+			desc = "FFFind files",
+		},
+	},
+}
