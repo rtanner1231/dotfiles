2a67a7c8a61818916c6f80a8599beb69e5f0dbf7
Richard Tanner <rtanner@myron.com>
2026-01-08 08:36:26 -0500

    Formatting

--- .config/nvim/lua/user/plugins/suiteqltools.lua
+++ .config/nvim/lua/user/plugins/suiteqltools.lua
@@ -1,34 +1,36 @@
 return {
-	"rtanner1231/suiteqltools.nvim",
-	dev = true,
-	keys = {
-		{ "<leader>ss", ":SuiteQL FormatQuery<cr>", desc = "NeoTree reveal" },
-		{ "<leader>sa", ":SuiteQL FormatFile<cr>", desc = "Format all SuiteQL in file" },
-		{ "<leader>se", ":SuiteQL ToggleEditor<cr>", desc = "Toggle SuiteQL editor" },
-		{ "<leader>sw", ":SuiteQL EditQuery<cr>", desc = "Edit the query under the cursor" },
-		{ "<leader>sc", ":NSConn SelectProfile<cr>", desc = "Change the active profile" },
-		{ "<leader>sh", ":SuiteQL History<cr>", desc = "View the query history" },
-		{ "<leader>sq", ":SuiteQL ShowTablePicker<cr>", desc = "Show the table picker" },
-		{ "<leader>sk", ":SuiteQL ShowFieldPicker<cr>", desc = "Show the field picker for the last table" },
-		{ "<leader>sl", ":SuiteQL ShowLastTableFieldPicker<cr>", desc = "Show the field picker for the last table" },
-	},
-	opts = {
-		sqlFormatter = {
-			keywordCase = "upper",
-			logicalOperatorNewline = "after",
-		},
-		queryRun = {
-			editorKeymap = {
-				toggleWindow = "<leader>sa",
-				runQuery = "<leader>sr",
-				toggleResultZoom = "<leader>sf",
-				nextPage = "<leader>sn",
-				previousPage = "<leader>sp",
-				toggleDisplayMode = "<leader>sm",
-			},
-			history = true,
-			completion = true,
-		},
-	},
-	ft = { "javascript", "typescript" },
+    "rtanner1231/suiteqltools.nvim",
+    dev = true,
+    keys = {
+        { "<leader>ss", ":SuiteQL FormatQuery<cr>",              desc = "NeoTree reveal" },
+        { "<leader>sa", ":SuiteQL FormatFile<cr>",               desc = "Format all SuiteQL in file" },
+        { "<leader>se", ":SuiteQL ToggleEditor<cr>",             desc = "Toggle SuiteQL editor" },
+        { "<leader>sw", ":SuiteQL EditQuery<cr>",                desc = "Edit the query under the cursor" },
+        { "<leader>sc", ":NSConn SelectProfile<cr>",             desc = "Change the active profile" },
+        { "<leader>sh", ":SuiteQL History<cr>",                  desc = "View the query history" },
+        { "<leader>sq", ":SuiteQL ShowTablePicker<cr>",          desc = "Show the table picker" },
+        { "<leader>sk", ":SuiteQL ShowFieldPicker<cr>",          desc = "Show the field picker for the last table" },
+        { "<leader>sl", ":SuiteQL ShowLastTableFieldPicker<cr>", desc = "Show the field picker for the last table" },
+        { "<leader>sj", ":SuiteQL EditTableUnderCursor<cr>",     desc = "Edit the table under the cursor in browser" },
+        { "<leader>su", ":SuiteQL ListTableUnderCursor<cr>",     desc = "List the table under the cursor in browser" },
+    },
+    opts = {
+        sqlFormatter = {
+            keywordCase = "upper",
+            logicalOperatorNewline = "after",
+        },
+        queryRun = {
+            editorKeymap = {
+                toggleWindow = "<leader>sa",
+                runQuery = "<leader>sr",
+                toggleResultZoom = "<leader>sf",
+                nextPage = "<leader>sn",
+                previousPage = "<leader>sp",
+                toggleDisplayMode = "<leader>sm",
+            },
+            history = true,
+            completion = true,
+        },
+    },
+    ft = { "javascript", "typescript" },
 }
