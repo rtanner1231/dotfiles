9243ef13fea4790c01072dd402d226d8e9acd8de
Richard Tanner <rtanner@myron.com>
2026-01-17 13:44:29 -0500

    Formatting

--- .config/nvim/lua/user/plugins/treesitter.lua
+++ .config/nvim/lua/user/plugins/treesitter.lua
@@ -27,9 +27,9 @@ return { -- Highlight, edit, and navigate code
 			--  the list of additional_vim_regex_highlighting and disabled languages for indent.
 			additional_vim_regex_highlighting = { "ruby" },
 		},
-		indent = { enable = true, disable = { "ruby" } },
+        indent = { enable = true, disable = { "ruby" } },
 	},
-	config = function(_, opts)
+    config = function(_, opts)
 		-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
 
 		---@diagnostic disable-next-line: missing-fields
